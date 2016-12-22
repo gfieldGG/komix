d74cce08a13d55d2096c10cca8d10e89027a74d5
gfield <github@gfield.de>
2016-12-22 20:39:24 +0100

    Started implementing routing

--- src/elements/ui/item-list.js
+++ src/elements/ui/item-list.js
@@ -3,14 +3,22 @@
 Polymer({
 	is: "item-list",
 	properties: {
-		items: Array
+		items: Array,
+		itemType: {
+			type: String,
+			observer: "_itemTypeChanged"
+		}
 	},
-	ready() {
+	_itemTypeChanged() {
 		const items = [];
+		const cover = this.itemType === "series"
+			? "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/349320/349320._SX1280_QL80_TTD_.jpg"
+			: "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/43074/DIG005396_1._SX1280_QL80_TTD_.jpg";
 
+		console.log(this.itemType);
 		for(let i = 0; i < 500; i++) {
 			items[i] = {
-				cover: "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/349320/349320._SX1280_QL80_TTD_.jpg",
+				cover,
 				title: "Saga",
 				year: 2000 + i
 			};
