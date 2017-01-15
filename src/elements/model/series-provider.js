cd9063d4b46dc71ef15ee212b69ddded39143d55
gfield <github@gfield.de>
2017-01-15 18:11:06 +0100

    implemented series-provider

--- src/elements/model/series-provider.js
+++ src/elements/model/series-provider.js
@@ -8,5 +8,37 @@ Polymer({
 			computed: "_computeSeries(_response)"
 		}
 	},
-	_computeSeries: response => response.series
+	_computeSeries: response => Array.from(response.comics.reduce((seriesMap, issue) => {
+		const id = `${issue.volume}@${issue.series}`;
+		let series = seriesMap.get(id);
+
+		if(series === undefined) {
+			series = {
+				startYearFromFirstIssue: false,
+				data: {
+					name: issue.series,
+					startYear: Infinity,
+					endYear: -Infinity,
+					coverIssue: undefined
+				}
+			};
+			seriesMap.set(id, series);
+		}
+
+		if(issue.issue === "1") {
+			series.data.startYear = issue.year;
+			series.startYearFromFirstIssue = true;
+			series.data.coverIssue = 1;
+		}
+		else if(issue.year < series.data.startYear && !series.startYearFromFirstIssue) {
+			series.data.startYear = issue.year;
+			series.data.coverIssue = Number(issue.issue);
+		}
+
+		if(issue.year > series.data.endYear) {
+			series.data.endYear = issue.year;
+		}
+
+		return series;
+	}, new Map()), ([, { data }]) => data)
 });
