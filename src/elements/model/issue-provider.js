aa917c2b5a6570d204deca2d5c805be2299eb306
gfield <github@gfield.de>
2017-01-18 01:20:32 +0100

    fixed issue-provider

--- src/elements/model/issue-provider.js
+++ src/elements/model/issue-provider.js
@@ -18,25 +18,15 @@ Polymer({
 		}
 	},
 	_computeIssues: response =>
-		Array.from(
-			response.comics.reduce(
-				(issueMap, issue) => {
-					const id = issue.id;
-					let issueObj = issueMap.get(id);
-
-					if(issueObj === undefined) {
-						issueObj = {
-							id: issue.id,
-							issueNumber: issue.issue,
-							title: `${issue.series} #${issue.issue}`,
-							series: issue.series,
-							year: issue.year,
-							pagecount: issue.page_count,
-							lastreadPage: Number(issue.lastread_page)
-						};
-					}
-					issueMap.set(id, issueObj);
-
-					return issueMap;
-				}, new Map()))
+		Array.from(response.comics.map(issue =>
+				issue = {
+					id: issue.id,
+					issueNumber: issue.issue,
+					title: `${issue.series} #${issue.issue}`,
+					series: issue.series,
+					year: issue.year,
+					pagecount: issue.page_count,
+					lastreadPage: Number(issue.lastread_page)
+				}
+		))
 });
--- src/elements/model/series-provider.js
+++ src/elements/model/series-provider.js
@@ -48,5 +48,5 @@ Polymer({
 			}
 
 			return seriesMap;
-		}, new Map()), ([, { data }]) => data)
+		}, new Map()), ([, data ]) => data)
 });
