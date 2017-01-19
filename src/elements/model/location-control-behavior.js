d0b3923fa7ecd36ed329d4ac8307ee38c5c37723
gfield <github@gfield.de>
2017-01-19 22:57:41 +0100

    started to rework route using singletonize

--- src/elements/model/location-control-behavior.js
+++ src/elements/model/location-control-behavior.js
@@ -1,13 +1,19 @@
 "use strict";
 
-komix.LocationControlBehavior = {
+komix.LocationControlBehavior = Polymer.singletonize({
 	properties: {
-		location: {
+		route: {
+			type: Object,
 			notify: true
 		}
 	},
+	addRoute(newRoute) {
+		if(this.route !== undefined)
+			throw new Error("Route is already initialized.");
+
+		this.route = newRoute;
+	},
 	gotoLibrary(view) {
-		this.set("location.view", view);
-		this.set("location.type", "library");
+		this.set("route.path", `/library/${view}`);
 	}
-};
+});
