156a369a8f68c97487d5650d1fc93137e6ee12d5
gfield <github@gfield.de>
2017-01-24 18:45:00 +0100

    started using max_height for cover images

--- src/elements/model/page-provider.js
+++ src/elements/model/page-provider.js
@@ -6,11 +6,18 @@ Polymer({
 		_path: {
 			computed: "_computePath(issueId, pageNumber)"
 		},
+		_params: {
+			computed: "_computeParams(maxHeight)"
+		},
 		issueId: Number,
 		pageNumber: Number,
+		maxHeight: Number,
 		pageUrl: {
 			notify: true
 		}
 	},
-	_computePath: (issueId, pageNumber) => `comic/${issueId}/page/${pageNumber}`
+	_computePath: (issueId, pageNumber) => `comic/${issueId}/page/${pageNumber}`,
+	_computeParams: maxHeight => ({
+		max_height: maxHeight
+	})
 });
